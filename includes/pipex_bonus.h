6baa4225cb76d97f995c4c319bcc741a3e8e4b43
Antoine Labalette <alabalet@student.42nice.fr>
2021-09-01 19:32:16 +0200

    [a] multi cmd

---
+++ includes/pipex_bonus.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alabalet <alabalet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/31 23:14:44 by alabalet          #+#    #+#             */
+/*   Updated: 2021/09/01 19:23:38 by alabalet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
+
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_param
+{
+	char	**paths;
+	char	**cmd;
+}			t_param;
+
+typedef struct s_vars
+{
+	t_param	*param;
+	int		**pipefd;
+	int		fd[2];
+	int		n;
+}			t_vars;
+
+char	**get_cmds(char **e, char *cmd);
+void	exec_cmd(int in_stream, int out_stream, t_param param, char **e);
+void	ft_free_matrix(t_param *param, int nb_param);
+void	ft_error(char *str, int type);
+#endif
--- srcs_bonus/main_bonus.c
+++ srcs_bonus/main_bonus.c
@@ -1,55 +1,94 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alabalet <alabalet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 23:13:35 by alabalet          #+#    #+#             */
-/*   Updated: 2021/09/01 00:39:53 by alabalet         ###   ########.fr       */
+/*   Updated: 2021/09/01 19:31:08 by alabalet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "pipex.h"
+#include "pipex_bonus.h"
 
-void	init_params(int fd[2], t_param param[2], char **av, char **e)
+void	init_fd(int fd[2], int n, char **av)
 {
 	fd[0] = open(av[1], O_RDONLY);
 	if (fd[0] == -1)
 		ft_error(av[1], 2);
-	fd[1] = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	fd[1] = open(av[n + 2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd[1] == -1)
-		ft_error(av[4], 2);
-	param[0].cmd = ft_split(av[2], ' ');
-	param[1].cmd = ft_split(av[3], ' ');
-	param[0].paths = get_cmds(e, param[0].cmd[0]);
-	param[1].paths = get_cmds(e, param[1].cmd[0]);
+		ft_error(av[n + 2], 2);
+}
+
+void	init_params(int n, t_param param[2], char **av, char **e)
+{
+	int	cpt;
+
+	cpt = -1;
+	while (++cpt < n)
+	{
+		param[cpt].cmd = ft_split(av[cpt + 2], ' ');
+		param[cpt].paths = get_cmds(e, param[cpt].cmd[0]);
+	}
+}
+
+void	init_vars(t_vars *v, int ac, char **av, char **e)
+{
+	int	cpt;
+
+	v->n = ac - 3;
+	cpt = 1;
+	while (++cpt < v->n + 2)
+		if (!ft_strlen(av[cpt]))
+			ft_error("Usage: ./pipex infile cmd1 cmd2 [... cmd_n] outfile\n", 0);
+	v->pipefd = malloc((v->n - 1) * sizeof(int *));
+	cpt = -1;
+	while (++cpt < v->n - 1)
+	{
+		v->pipefd[cpt] = malloc(2 * sizeof(int));
+		pipe(v->pipefd[cpt]);
+	}
+	init_fd(v->fd, v->n, av);
+	v->param = malloc(v->n * sizeof(t_param));
+	init_params(v->n, v->param, av, e);
 }
 
 int	main(int ac, char **av, char **e)
 {
-	t_param	param[2];
-	int		fd[2];
-	int		pipefd[2];
+	t_vars	v;
+	int		cpt;
 
-	if (ac != 5 || !ft_strlen(av[2]) || !ft_strlen(av[3]))
-		ft_error("Usage: ./pipex infile cmd1 cmd2 outfile\n", 0);
-	pipe(pipefd);
-	init_params(fd, param, av, e);
+	if (ac < 5)
+		ft_error("Usage: ./pipex infile cmd1 cmd2 [... cmd_n] outfile\n", 0);
+	init_vars(&v, ac, av, e);
 	if (!fork())
 	{
-		close(pipefd[0]);
-		exec_cmd(fd[0], pipefd[1], param[0], e);
+		close(v.pipefd[0][0]);
+		exec_cmd(v.fd[0], v.pipefd[0][1], v.param[0], e);
 	}
 	waitpid(-1, 0, 0);
+	cpt = 0;
+	while (++cpt < v.n - 1)
+	{
+		if (!fork())
+		{
+			close(v.pipefd[cpt - 1][1]);
+			close(v.pipefd[cpt][0]);
+			exec_cmd(v.pipefd[cpt - 1][0], v.pipefd[cpt][1], v.param[cpt], e);
+		}
+		close(v.pipefd[cpt - 1][1]);
+		waitpid(-1, 0, 0);
+	}
 	if (!fork())
 	{
-		close(pipefd[1]);
-		exec_cmd(pipefd[0], fd[1], param[1], e);
+		close(v.pipefd[v.n - 2][1]);
+		exec_cmd(v.pipefd[v.n - 2][0], v.fd[1], v.param[v.n - 1], e);
 	}
-	close(pipefd[1]);
+	close(v.pipefd[v.n - 2][1]);
 	waitpid(-1, 0, 0);
-	ft_free_matrix(param, 2);
+	ft_free_matrix(v.param, 2);
 	return (0);
 }
